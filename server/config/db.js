6db1d4b12dbc23a8d85665f0f8f29899fdee30f7
sanskar <sanskargupta37081@gmail.com>
2024-08-02 15:45:02 +0530

    Created server folder

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000 // Increase the timeout
+    });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
